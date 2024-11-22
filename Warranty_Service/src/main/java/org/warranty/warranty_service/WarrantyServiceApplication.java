a173f7a3b09352b7df18327abb6b8bab4107ed22
sachinjangam29 <Sachinjagannathjangam29@gmail.com>
2024-11-22 21:58:01 +0530

    test cases for all the required methods till now.

--- Warranty_Service/src/main/java/org/warranty/warranty_service/WarrantyServiceApplication.java
+++ Warranty_Service/src/main/java/org/warranty/warranty_service/WarrantyServiceApplication.java
@@ -2,8 +2,10 @@ package org.warranty.warranty_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WarrantyServiceApplication {
 
     public static void main(String[] args) {
